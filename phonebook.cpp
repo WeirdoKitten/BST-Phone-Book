62dc60c3e712b7a0734b5d7bacfe5720e4a0aa6e
bamoebin <farrasaer@gmail.com>
2024-05-30 08:31:58 +0700

    update

--- phonebook.cpp
+++ phonebook.cpp
@@ -2,10 +2,12 @@
 
 node *root = NULL;
 
-node *createNode(char inputName[50], char inputNumber[13]) {
+node *createNode(char inputName[50], char inputNumber[13])
+{
     node *newNode = (node *)malloc(sizeof(node));
     numberList *newNumber = (numberList *)malloc(sizeof(numberList));
-    if (newNode == NULL || newNumber == NULL) {
+    if (newNode == NULL || newNumber == NULL)
+    {
         return NULL;
     }
     strcpy(newNode->name, inputName);
@@ -17,37 +19,46 @@ node *createNode(char inputName[50], char inputNumber[13]) {
     return newNode;
 }
 
-void addPhoneNumber(node *personNode, char inputNumber[13]) {
+void addPhoneNumber(node *personNode, char inputNumber[13])
+{
     numberList *newNumber = (numberList *)malloc(sizeof(numberList));
-    if (newNumber == NULL) {
+    if (newNumber == NULL)
+    {
         return;
     }
     strcpy(newNumber->phoneNumber, inputNumber);
     newNumber->next = NULL;
 
     numberList *current = personNode->phoneNumberS;
-    while (current->next != NULL) {
+    while (current->next != NULL)
+    {
         current = current->next;
     }
     current->next = newNumber;
 }
 
-void toCapitalizeCase(char str[]) {
+void toCapitalizeCase(char str[])
+{
     int len = strlen(str);
     int capitalizeNext = 1;
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
+    {
         str[i] = tolower(str[i]);
-        if (capitalizeNext && isalpha(str[i])) {
+        if (capitalizeNext && isalpha(str[i]))
+        {
             str[i] = toupper(str[i]);
             capitalizeNext = 0;
-        } else if (str[i] == ' ') {
+        }
+        else if (str[i] == ' ')
+        {
             capitalizeNext = 1;
         }
     }
 }
 
-void insertData(char inputName[50], char inputNumber[13]) {
+void insertData(char inputName[50], char inputNumber[13])
+{
     char formattedName[50];
     strcpy(formattedName, inputName);
     toCapitalizeCase(formattedName);
@@ -55,38 +66,51 @@ void insertData(char inputName[50], char inputNumber[13]) {
     node *currNode = root;
     node *prevNode = NULL;
 
-    if (root == NULL) {
+    if (root == NULL)
+    {
         root = createNode(formattedName, inputNumber);
         return;
     }
 
-    while (currNode != NULL) {
+    while (currNode != NULL)
+    {
         prevNode = currNode;
-        if (strcmp(currNode->name, formattedName) == 0) {
+        if (strcmp(currNode->name, formattedName) == 0)
+        {
             addPhoneNumber(currNode, inputNumber);
             return;
-        } else if (strcmp(currNode->name, formattedName) > 0) {
+        }
+        else if (strcmp(currNode->name, formattedName) > 0)
+        {
             currNode = currNode->left;
-        } else {
+        }
+        else
+        {
             currNode = currNode->right;
         }
     }
 
     node *newNode = createNode(formattedName, inputNumber);
-    if (strcmp(prevNode->name, formattedName) > 0) {
+    if (strcmp(prevNode->name, formattedName) > 0)
+    {
         prevNode->left = newNode;
-    } else {
+    }
+    else
+    {
         prevNode->right = newNode;
     }
 }
 
-void printAllNode(node *currNode) {
-    if (currNode != NULL) {
+void printAllNode(node *currNode)
+{
+    if (currNode != NULL)
+    {
         printAllNode(currNode->left);
         printf("===================================================\n");
         printf("Nama          : %s\n", currNode->name);
         numberList *numList = currNode->phoneNumberS;
-        while (numList != NULL) {
+        while (numList != NULL)
+        {
             printf("Nomor Telepon : %s\n", numList->phoneNumber);
             numList = numList->next;
         }
@@ -95,11 +119,13 @@ void printAllNode(node *currNode) {
     }
 }
 
-void inputData() {
+void inputData()
+{
     char inputNama[50];
     char inputNumber[13];
 
-    while ((getchar()) != '\n');
+    while ((getchar()) != '\n')
+        ;
     printf("Inputkan Nama : ");
     scanf("%[^\n]%*c", inputNama);
     printf("Inputkan Nomor Telepon: ");
@@ -107,42 +133,52 @@ void inputData() {
 
     insertData(inputNama, inputNumber);
 
-    saveToFile(root, "data.txt"); 
+    saveToFile(root, "data.txt");
 }
 
-
-void search(node *root, char inputName[50]) {
+void search(node *root, char inputName[50])
+{
     char formattedName[50];
     strcpy(formattedName, inputName);
     toCapitalizeCase(formattedName);
 
-    if (root != NULL) {
-        if (strstr(root->name, formattedName)) {
+    if (root != NULL)
+    {
+        if (strstr(root->name, formattedName))
+        {
             printf("===================================================\n");
             printf("Nama          : %s\n", root->name);
             numberList *numList = root->phoneNumberS;
-            while (numList != NULL) {
+            while (numList != NULL)
+            {
                 printf("Nomor Telepon : %s\n", numList->phoneNumber);
                 numList = numList->next;
             }
             printf("===================================================\n\n");
         }
-        if (strcmp(formattedName, root->name) < 0) {
-            search(root->left, formattedName); 
-        } else {
-            search(root->right, formattedName); 
+        if (strcmp(formattedName, root->name) < 0)
+        {
+            search(root->left, formattedName);
+        }
+        else
+        {
+            search(root->right, formattedName);
         }
     }
 }
 
-void saveToFile(node *root, const char *filename) {
+void saveToFile(node *root, const char *filename)
+{
     FILE *file = fopen(filename, "w");
-    if (file != NULL) {
-        if (root != NULL) {
+    if (file != NULL)
+    {
+        if (root != NULL)
+        {
             saveToFile(root->left, filename);
             fprintf(file, "Nama: %s\n", root->name);
             numberList *numList = root->phoneNumberS;
-            while (numList != NULL) {
+            while (numList != NULL)
+            {
                 fprintf(file, "Nomor Telepon: %s\n", numList->phoneNumber);
                 numList = numList->next;
             }
@@ -150,16 +186,18 @@ void saveToFile(node *root, const char *filename) {
             saveToFile(root->right, filename);
         }
         fclose(file);
-    } else {
+    }
+    else
+    {
         printf("Gagal membuka file\n");
     }
 }
 
-
-
-void loadFromFile(const char *filename) {
+void loadFromFile(const char *filename)
+{
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         return;
     }
 
@@ -167,9 +205,12 @@ void loadFromFile(const char *filename) {
     char name[50];
     char phoneNumber[13];
 
-    while (fgets(line, sizeof(line), file)) {
-        if (sscanf(line, "Nama: %[^\n]", name) == 1) {
-            while (fgets(line, sizeof(line), file) && sscanf(line, "Nomor Telepon: %[^\n]", phoneNumber) == 1) {
+    while (fgets(line, sizeof(line), file))
+    {
+        if (sscanf(line, "Nama: %[^\n]", name) == 1)
+        {
+            while (fgets(line, sizeof(line), file) && sscanf(line, "Nomor Telepon: %[^\n]", phoneNumber) == 1)
+            {
                 insertData(name, phoneNumber);
             }
         }
@@ -178,25 +219,32 @@ void loadFromFile(const char *filename) {
     fclose(file);
 }
 
-node* findMin(node* root) {
-    while (root->left != NULL) root = root->left;
+node *findMin(node *root)
+{
+    while (root->left != NULL)
+        root = root->left;
     return root;
 }
 
-node* deleteContact(node *root, char inputName[50]) {
-    if (root == NULL) return root;
+node *deleteContact(node *root, char inputName[50])
+{
+    if (root == NULL)
+        return root;
 
     if (strcmp(inputName, root->name) < 0)
         root->left = deleteContact(root->left, inputName);
     else if (strcmp(inputName, root->name) > 0)
         root->right = deleteContact(root->right, inputName);
-    else {
-        if (root->left == NULL) {
+    else
+    {
+        if (root->left == NULL)
+        {
             node *temp = root->right;
             free(root);
             return temp;
         }
-        else if (root->right == NULL) {
+        else if (root->right == NULL)
+        {
             node *temp = root->left;
             free(root);
             return temp;
@@ -208,14 +256,15 @@ node* deleteContact(node *root, char inputName[50]) {
         root->right = deleteContact(root->right, temp->name);
     }
 
-    
     saveToFile(root, "data.txt");
 
     return root;
 }
 
-void updateContact(node *root, const char *inputName, const char *newName, char newNumber[13]) {
-    if (root == NULL) {
+void updateContact(node *root, const char *inputName, const char *newName, char newNumber[13])
+{
+    if (root == NULL)
+    {
         printf("Kontak tidak ditemukan\n");
         return;
     }
@@ -224,15 +273,21 @@ void updateContact(node *root, const char *inputName, const char *newName, char 
         updateContact(root->left, inputName, newName, newNumber);
     else if (strcmp(inputName, root->name) > 0)
         updateContact(root->right, inputName, newName, newNumber);
-    else {
-        if (newName != NULL && strlen(newName) > 0) {
+    else
+    {
+        if (newName != NULL && strlen(newName) > 0)
+        {
             strcpy(root->name, newName);
         }
-        if (newNumber != NULL && strlen(newNumber) > 0) {
+        if (newNumber != NULL && strlen(newNumber) > 0)
+        {
             numberList *numList = root->phoneNumberS;
-            if (numList != NULL) {
+            if (numList != NULL)
+            {
                 strcpy(numList->phoneNumber, newNumber);
-            } else {
+            }
+            else
+            {
                 addPhoneNumber(root, newNumber);
             }
         }
--- phonebook.h
+++ phonebook.h
@@ -21,77 +21,65 @@ typedef struct node {
 
 extern node* root;
 
+/* 
+    Tujuan Prosedur : membuat node baru pada binary search tree yang berisi nama dan nomor telepon yang diinputkan.
+*/
 node* createNode(char inputName[50], char inputNumber[13]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : memasukkan data nama dan nomor telepon ke dalam binary search tree. Jika nama sudah ada, nomor telepon baru akan ditambahkan pada daftar nomor telepon yang dimiliki oleh node tersebut.
+*/
 void insertData(char inputName[50], char inputNumber[13]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : menambahkan nomor telepon baru pada daftar nomor telepon yang dimiliki oleh node tertentu.
+*/
 void addPhoneNumber(node *personNode, char inputNumber[13]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
-void inputData();
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
+/* 
+    Tujuan Prosedur :  meminta input dari pengguna berupa nama dan nomor telepon, kemudian memanggil fungsi insertData untuk memasukkan data tersebut ke dalam binary search tree.
 */
+void inputData(); 
 
+/* 
+    Tujuan Prosedur : mencetak semua data nama dan nomor telepon yang tersimpan dalam binary search tree secara inorder traversal.
+*/
 void printAllNode(node *currNode);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : mencari node berdasarkan nama yang diinputkan, kemudian mencetak nama dan daftar nomor telepon yang dimilikinya.
+*/
 void search(node *root, char inputName[50]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : menyimpan semua data nama dan nomor telepon yang tersimpan dalam binary search tree ke dalam file eksternal.
+*/
 void saveToFile(node *root, const char *filename);
 
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : memuat data nama dan nomor telepon dari file eksternal ke dalam binary search tree.
+*/
 void loadFromFile(const char *filename);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur :  mengubah format penulisan nama menjadi kapital pada setiap huruf pertama kata.
+*/
 void toCapitalizeCase(char str[]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : mencari node dengan nilai paling kecil dalam subtree kanan dari node tertentu, yang digunakan dalam proses penghapusan node.
+*/
 node* findMin(node* root);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
+/* 
+    Tujuan Prosedur : menghapus node berdasarkan nama yang diinputkan dari binary search tree.
+*/
 node* deleteContact(node *root, char inputName[50]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
-*/
 
-void updateContact(node *root, const char *inputName, const char *newName, char newNumber[13]);
-/*
-    Penulis         : 
-    Tujuan Prosedur : 
+/* 
+    Tujuan Prosedur : memperbarui data nama dan/atau nomor telepon pada node tertentu dalam binary search tree. 
 */
+void updateContact(node *root, const char *inputName, const char *newName, char newNumber[13]);
 
 #endif
